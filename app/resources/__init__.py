cb0f3e442e3f43255381e5d0b19e4f804f86a8bb
Darren Maczka <dmaczka@vt.edu>
2015-02-14 16:10:40 -0500

    rename variable to metric

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,5 +1,6 @@
 from site import SiteResource
 from instrument import InstrumentResource
 from sensor import SensorResource
-from variable import VariableResource
+from metric import MetricResource
 from observation import ObservationResource
+from unit import UnitResource
