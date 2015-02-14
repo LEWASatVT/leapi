060481095c2ac5e723660946bc417858ae3a419f
Darren Maczka <dmaczka@vt.edu>
2015-02-14 09:26:38 -0500

    populate instruments, sensors

--- test/populate.py
+++ test/populate.py
@@ -1,10 +1,12 @@
 import sys
 
+sys.path.append('./')
 sys.path.append('../')
 
 from app import db, models
 
 variables = [ ('flow','water'),
+              ('velocity','water'),
               ('temperature','water'),
               ('temperature','air'),
               ('dissolved oxygen','water'),
@@ -20,18 +22,41 @@ units = [('degrees celsius', 'C', 'temperature'),
          ('inches of mercury','in', 'pressure'),
          ('percent humidity', '%', 'percentage')]
 
-instruments = [ ('weather station'
+instruments = [ ('weather station', 'stroubles1'),
+                ('sonde', 'stroubles1'),
+                ('argonaut', 'stroubles1')]
+
+sensors = [ ('temperature', 'weather station', ('temperature', 'air')),
+            ('velocity', 'argonaut', ('velocity', 'water')),
+            ('temperature', 'sonde', ('temperature', 'water'))]
+
+cinstruments = [ (i.name,i.site_id) for i in models.Instrument.query.all() ]
 cvariables = [ (v.name,v.medium) for v in models.Variable.query.all() ]
-cunits = [ (u.name, u.abbv, u.type) for u in modes.Units.query.all() ]
+cunits = [ (u.name, u.abbv, u.type) for u in models.Unit.query.all() ]
+csensors = [ (s.name, s.instrument) for s in models.Sensor.query.all() ]
+
+for (name,site_id) in instruments:
+    if (name,site_id) not in cinstruments:
+        site = models.Site.query.get(site_id)
+        i = models.Instrument(name=unicode(name))
+        i.site = site
+        db.session.add(i)
 
 for (name,medium) in variables:
     if (name,medium) not in cvariables:
         v = models.Variable(name=unicode(name), medium=unicode(medium))
         db.session.add(v)
 
-for (name,abbv,utype) for units:
+for (name,abbv,utype) in units:
     if (name,abbv,utype) not in cunits:
         u = models.Unit(name=unicode(name), abbv=unicode(abbv), type=unicode(utype))
         db.session.add(u)
+
+for (name, instrument, (vname, vmedium)) in sensors:
+    if (name,instrument) not in csensors:
+        s = models.Sensor(name)
+        s.instrument = models.Instrument.query.filter_by(name=instrument).first()
+        s.variable = models.Variable.query.filter_by(name=vname, medium=vmedium).first()
+        db.session.add(s)
 
 db.session.commit()
