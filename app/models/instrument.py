b3386f670db85b6157b8e05b94ad6ba1d010cec5
Darren Maczka <dmaczka@vt.edu>
2015-02-22 12:09:57 -0500

    remove uniqueness constraint on name, make it on (site_id,name)

--- app/models/instrument.py
+++ app/models/instrument.py
@@ -1,10 +1,12 @@
 from app import db
+from sqlalchemy import UniqueConstraint
 
 class Instrument(db.Model):
     __tablename__ = 'instruments'
+    __table_args__ = (UniqueConstraint('site_id', 'name',name='instrument_site_name_key'),)
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode, unique=True)
+    name = db.Column(db.Unicode)
     model = db.Column(db.Unicode)
     manufacturer = db.Column(db.Unicode)
     site_id = db.Column(db.Unicode, db.ForeignKey('sites.id'))
