f40836446049e598dce66768ce863264a962f9a7
Darren Maczka <dmaczka@vt.edu>
2015-02-28 15:58:50 -0500

    put back CORS
    ok, so yes, this is needed, even though
    it doesn't look like it's "doing" anything.

--- leapi/__init__.py
+++ leapi/__init__.py
@@ -2,9 +2,12 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restful import Api
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+cors = CORS(app)
+
 db = SQLAlchemy(app)
 api = Api(app)
 
