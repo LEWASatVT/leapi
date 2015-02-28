a9c96877ed32e5be2d1a49c53a99bdc0e8e79346
Darren Maczka <dmaczka@vt.edu>
2015-02-28 10:03:32 -0500

    remove import of unused module

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask
-from flask.ext.cors import CORS
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restful import Api
 
 app = Flask(__name__)
-cors = CORS(app)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 api = Api(app)
