0f0dc6e4cfd957fb7dbcdb26ca951fce918cb69b
Darren Maczka <dmaczka@vt.edu>
2015-03-08 14:10:55 -0400

    add logging

--- leapi/__init__.py
+++ leapi/__init__.py
@@ -11,4 +11,19 @@ cors = CORS(app)
 db = SQLAlchemy(app)
 api = Api(app)
 
+if not app.debug:
+    import logging
+    from logging import handlers
+    file_handler = handlers.RotatingFileHandler('/var/log/flask/leapi.log')
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+
+    from logging import Formatter
+    file_handler.setFormatter(Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'
+    ))
+
+
 import leapi.views, leapi.models, leapi.resources
+import leapi.archive
